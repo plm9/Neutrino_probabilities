6a124e11f6eb05d2e8fdd250dfb16501064505e3
Achilleas Patsias <s6acpats@uni-bonn.de>
2022-07-22 14:09:39 +0200

    Major Update transforming Prob_funcs without sympy

--- Prob_functions.py
+++ Prob_functions.py
@@ -1,8 +1,10 @@
 import numpy as np
 from sympy import *
 
-from my_functions import *
+import package_func.my_functions as mf
 
+L=Symbol("L",real=True,positive=True)
+E_nu=Symbol("E_nu",real=True)
 
 def Prob_a_to_b(a,b):
     """ 
@@ -15,17 +17,17 @@ def Prob_a_to_b(a,b):
     a function of the mixing angles ,the mass differences, the distance traveled and the 
     energy of the neutrino.
     """
-    index_a,index_b=flavor_to_index(a,b)
+    index_a,index_b=mf.flavor_to_index(a,b)
 
-    U=PMNS_param_matrix()
+    U=mf.PMNS_param_matrix()
 
     f=U[index_b,0]*conjugate(U[index_a,0])*U[index_a,1]*conjugate(U[index_b,1])
     s=U[index_b,0]*conjugate(U[index_a,0])*U[index_a,2]*conjugate(U[index_b,2])
     t=U[index_b,1]*conjugate(U[index_a,1])*U[index_a,2]*conjugate(U[index_b,2])
 
-    first=re(f)*sin((D_m_21*L)/(4*E_nu))**2
-    second=re(s)*sin((D_m_31*L)/(4*E_nu))**2
-    third=re(t)*sin((D_m_32*L)/(4*E_nu))**2
+    first=re(f)*sin((mf.D_m_21*L)/(4*E_nu))**2
+    second=re(s)*sin((mf.D_m_31*L)/(4*E_nu))**2
+    third=re(t)*sin((mf.D_m_32*L)/(4*E_nu))**2
 
     if a==b:
         return 1-4*(first+second+third)
@@ -33,17 +35,17 @@ def Prob_a_to_b(a,b):
         return -4*(first+second+third)
 
 def Prob_anti_a_to_anti_b(a,b):
-    index_a,index_b=flavor_to_index(a,b)
+    index_a,index_b=mf.flavor_to_index(a,b)
 
-    U=PMNS_param_matrix()
+    U=mf.PMNS_param_matrix()
 
     f=conjugate(U[index_b,0])*U[index_a,0]*conjugate(U[index_a,1])*U[index_b,1]
     s=conjugate(U[index_b,0])*U[index_a,0]*conjugate(U[index_a,2])*U[index_b,2]
     t=conjugate(U[index_b,1])*U[index_a,1]*conjugate(U[index_a,2])*U[index_b,2]
 
-    first=re(f)*sin((D_m_21*L)/(4*E_nu))**2
-    second=re(s)*sin((D_m_31*L)/(4*E_nu))**2
-    third=re(t)*sin((D_m_32*L)/(4*E_nu))**2
+    first=re(f)*sin((mf.D_m_21*L)/(4*E_nu))**2
+    second=re(s)*sin((mf.D_m_31*L)/(4*E_nu))**2
+    third=re(t)*sin((mf.D_m_32*L)/(4*E_nu))**2
 
     if a==b:
         return 1-4*(first+second+third)
@@ -51,31 +53,31 @@ def Prob_anti_a_to_anti_b(a,b):
         return -4*(first+second+third)
 
 def Prob_a_to_b_alt(a,b):
-    index_a,index_b=flavor_to_index(a,b)
+    index_a,index_b=mf.flavor_to_index(a,b)
     
-    U=PMNS_param_matrix()
+    U=mf.PMNS_param_matrix()
 
     fst_rhs=0
-    U=PMNS_param_matrix()
+    U=mf.PMNS_param_matrix()
     for i in [0,1,2]:
-        fst_rhs+=(norm(U[index_b,i])**2)* norm(U[index_a,i])**2
+        fst_rhs+=(mf.norm(U[index_b,i])**2)* mf.norm(U[index_a,i])**2
 
     scd_rhs=0
     for i in [0,1,2]:
         for j in [0,1,2]:
             if i>j:
-                scd_rhs+=U[index_b,i]*conjugate(U[index_a,i])*conjugate(U[index_b,j])*U[index_a,j]*cos((D_mass_param(i+1,j+1))*L/(2*E_nu))
+                scd_rhs+=U[index_b,i]*conjugate(U[index_a,i])*conjugate(U[index_b,j])*U[index_a,j]*cos((mf.D_mass_param(i+1,j+1))*L/(2*E_nu))
 
     return fst_rhs+(2*re(scd_rhs))
 
 
 def Prob_a_to_b_General(a,b,type):
-    index_a,index_b=flavor_to_index(a,b)
+    index_a,index_b=mf.flavor_to_index(a,b)
 
     if type == "anti":
-        U=conjugate(PMNS_param_matrix())
+        U=conjugate(mf.PMNS_param_matrix())
     else:
-        U=PMNS_param_matrix()
+        U=mf.PMNS_param_matrix()
 
     re_sum=0
     im_sum=0
@@ -83,19 +85,19 @@ def Prob_a_to_b_General(a,b,type):
         for j in [0,1,2]:
             if i>j:
                 term=U[index_b,i]*conjugate(U[index_a,i])*conjugate(U[index_b,j])*U[index_a,j]
-                re_sum+=re(term)*(sin((D_mass_param(i+1,j+1))*L/(4*E_nu)))**2
-                im_sum+=im(term)*(sin((D_mass_param(i+1,j+1))*L/(2*E_nu)))
+                re_sum+=re(term)*(sin((mf.D_mass_param(i+1,j+1))*L/(4*E_nu)))**2
+                im_sum+=im(term)*(sin((mf.D_mass_param(i+1,j+1))*L/(2*E_nu)))
                 del term
     
-    return delta_Kro(a,b)-4*re_sum-2*im_sum
+    return mf.delta_Kro(a,b)-4*re_sum-2*im_sum
 
 def Prob_a_to_b_Gen_MSW(a,b,type="normal"):
-    index_a,index_b=flavor_to_index(a,b)
+    index_a,index_b=mf.flavor_to_index(a,b)
 
     if type == "anti":
-        U=conjugate(PMNS_param_matrix())
+        U=conjugate(mf.PMNS_param_matrix())
     else:
-        U=PMNS_param_matrix()
+        U=mf.PMNS_param_matrix()
 
     re_sum=0
     im_sum=0
@@ -103,13 +105,13 @@ def Prob_a_to_b_Gen_MSW(a,b,type="normal"):
         for j in [0,1,2]:
             if i>j:
                 if i+1==1 and j+1==3:
-                    mass_term=MSW_Dmass(i+1,j+1)
+                    mass_term=mf.MSW_Dmass(i+1,j+1)
                 else:
-                    mass_term=D_mass_param(i+1,j+1)
+                    mass_term=mf.D_mass_param(i+1,j+1)
                 term=U[index_b,i]*conjugate(U[index_a,i])*conjugate(U[index_b,j])*U[index_a,j]
                 re_sum+=re(term)*(sin((mass_term)*L/(4*E_nu)))**2
                 im_sum+=im(term)*(sin((mass_term)*L/(2*E_nu)))
                 del term
-    returner=delta_Kro(a,b)-4*re_sum-2*im_sum
+    returner=mf.delta_Kro(a,b)-4*re_sum-2*im_sum
     return returner
 
