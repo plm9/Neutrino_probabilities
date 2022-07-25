bf2282fdb1dd7cf714ec5ae13b13b0da375c6337
Achilleas Patsias <s6acpats@uni-bonn.de>
2022-07-25 21:02:30 +0200

    Commit before correcting everything

--- Organized/use_functions.py
+++ Organized/use_functions.py
@@ -1,7 +1,9 @@
+from turtle import title
 import numpy as np
 import package_func.my_functions as mf
 import matplotlib.pyplot as plt
 import package_func.Prob_funcs as pf
+from sympy import rotations
 
 #Constants
 R_earth=6371 #km
@@ -58,18 +60,22 @@ def main():
 
     flavors=["e","mu","tau"]
 
-    data_list=[]
-    for a in ["mu"]:
-
-        MSW_prob=np.empty([N,N])    
-        MSW_prob_anti=np.empty([N,N])
+    neutrinos=[r"$\nu_e$",r"$\nu_\mu$",r"$\nu_\tau$"]
+    anti_neutrinos=[r"$\bar{\nu}_e$",r"$\bar{\nu}_\mu$",r"$\bar{\nu}_\tau$"]
+    nu_index=0
+    for a in flavors:
+        data_list=[]
+        titles=[]
 
         for b in flavors:
             #Prob=np.empty([N,N])
             #Prob_cp=np.empty([N,N])
 
-            Total_prob=np.empty([N,N])    
-            Total_prob_anti=np.empty([N,N])
+            #Total_prob=np.empty([N,N])    
+            #Total_prob_anti=np.empty([N,N])
+
+            MSW_prob=np.empty([N,N])    
+            MSW_prob_anti=np.empty([N,N])
 
             for i in range(N):
                 for j in range(N):
@@ -85,31 +91,63 @@ def main():
 
                     #Prob[i,j]=CP_difference_with_35("e","mu",Cs,Eng)
                     #Prob_cp[i,j]=CP_difference_with_P_cp(Cs,Eng)
-        data_list.append(MSW_prob,MSW_prob_anti)           
+            data_list.append(MSW_prob)
+            data_list.append(MSW_prob_anti)
+            titles.append(a+"->"+b)
 
-    data_list=[MSW_prob_anti,MSW_prob,MSW_prob_anti-MSW_prob,Total_prob_anti,Total_prob,Total_prob_anti-Total_prob]#Prob[MSW_prob_anti,Prob_cp,Total_prob,MSW_prob]#Prob
-    titles=["MSW_anti","MSW","MSW anti-normal","General_anti","General","General anti-normal"]#"Eq_35""P_cp"
+
+    #data_list=[MSW_prob_anti,MSW_prob,MSW_prob_anti-MSW_prob,Total_prob_anti,Total_prob,Total_prob_anti-Total_prob]#Prob[MSW_prob_anti,Prob_cp,Total_prob,MSW_prob]#Prob
+    #titles=["MSW_anti","MSW","MSW anti-normal","General_anti","General","General anti-normal"]#"Eq_35""P_cp"
     
 
-    fig, ax = plt.subplots(2, 3,figsize=(16, 18),sharex="col",sharey="row")
-    indexer=0
-    
-    for i in range(2):
+        fig, ax = plt.subplots(2, 3,figsize=(16, 18),sharex="col",sharey="row")
+        indexer=0
+        title_index=0
+        for i in range(3):
+            for j in range(2):
+                data = data_list[indexer]
+                im = ax[j, i].pcolormesh(En,Cz,data,cmap="hot",vmin=0,vmax=1)
+                plt.colorbar(im, ax=ax[j, i])
+                ax[j,i].set_xscale("log")
+                if indexer%2==0:
+                    ax[j,i].set_title(titles[title_index])
+                    title_index+=1  
+                indexer+=1
+        
+        ax[0,0].set_ylabel(neutrinos[nu_index],fontsize=16)
+        ax[1,0].set_ylabel(anti_neutrinos[nu_index],fontsize=16)
+        fig.text(0.07,0.5,r"$\cos(\theta)$",va='center',rotation="vertical",fontsize=16)
+        fig.text(0.93,0.5,r"$P(\nu)$",va='center',rotation="vertical",fontsize=16)
+        fig.text(0.5, 0.04, r"$E_\nu$(GeV)", ha='center',fontsize=16)
+
+        #plt.savefig("Oscillation_probs_MSW_{}.pdf".format(a))
+        #plt.show()
+
+        ## CP violation plots
+        fig1, ax1 = plt.subplots(1, 3,figsize=(10, 5),sharex="col",sharey="row")
+        indexer_CP=0
+        title_index=0
+        #for i in range(1):
         for j in range(3):
-            data = data_list[indexer]
-            im = ax[i, j].pcolormesh(En,Cz,data,cmap="hot")
-            plt.colorbar(im, ax=ax[i, j])
-            ax[i,j].set_xscale("log")
-            ax[i,j].set_title(titles[indexer])
-            if i==1:
-                ax[i,j].set_xlabel("E(GeV)")
-            indexer+=1
+            data = data_list[indexer_CP]-data_list[indexer_CP+1]
+            im = ax1[j].pcolormesh(En,Cz,data,cmap="hot",vmin=0,vmax=0.5)
+            plt.colorbar(im, ax=ax1[j])
+            ax1[j].set_xscale("log")
+            if indexer_CP%2==0:
+                ax1[j].set_title(titles[title_index])
+                title_index+=1  
+            indexer_CP+=2
+        
+        ax1[0].set_ylabel(neutrinos[nu_index]+"-"+anti_neutrinos[nu_index],fontsize=14)
+        fig1.text(0.01,0.5,r"$\cos(\theta)$",va='center',rotation="vertical",fontsize=14)
+        fig1.text(0.93,0.5,r"$P(\nu)-P(\bar{\nu})$",va='center',rotation="vertical",fontsize=14)
+        fig1.text(0.5, 0.02, r"$E_\nu$(GeV)", ha='center',fontsize=14)
 
-    #plt.savefig("Oscillation_probs_MSW_and_eq34.pdf")
-
-    ax[0,0].set_ylabel(r"$\cos(\theta)$")
-    ax[1,0].set_ylabel(r"$\cos(\theta)$")
-    plt.show()
+        plt.savefig("neutrinos-anti_{}.pdf".format(a))
+        #plt.show()
+        nu_index+=1
+        del data_list,titles
+        #break
 
 
 if __name__=="__main__":
